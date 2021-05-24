aee477714da422bef622d0e09b33de01d6c6e357
Roland König <roland.koenig@rolandk.de>
2021-05-24 06:37:39 +0200

    Added HappyCoding.AvaloniaSkia2DRendering

---
+++ 2021/HappyCoding.AvaloniaSkia2DRendering/HappyCoding.AvaloniaSkia2DRendering/CustomSkiaRenderer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia;
+using Avalonia.Platform;
+using Avalonia.Rendering.SceneGraph;
+using Avalonia.Skia;
+using SkiaSharp;
+
+namespace HappyCoding.AvaloniaSkia2DRendering
+{
+    public class CustomSkiaRenderer : ICustomDrawOperation, IDisposable
+    {
+        private bool _disposed;
+
+        private SKPaint? _background;
+
+        /// <inheritdoc />
+        public Rect Bounds { get; set; }
+
+        public CustomSkiaRenderer()
+        {
+            this.Bounds = Rect.Empty;
+
+            _background = new SKPaint()
+            {
+                Color = new SKColor(150, 200, 255),
+                Style = SKPaintStyle.Fill
+            };
+            _disposed = false;
+        }
+
+        /// <inheritdoc />
+        public void Dispose()
+        {
+            _background?.Dispose();
+            _background = null;
+
+            _disposed = true;
+        }
+
+        /// <inheritdoc />
+        public bool HitTest(Point p)
+        {
+            return false;
+        }
+
+        /// <inheritdoc />
+        public void Render(IDrawingContextImpl context)
+        {
+            if (_disposed){ return; }
+            if (this.Bounds == Rect.Empty) { return; }
+
+            if (!(context is ISkiaDrawingContextImpl skiaContext))
+            {
+                // Cancel here...
+                // We do only support custom rendering with Skia
+                return;
+            }
+
+            var skiaCanvas = skiaContext.SkCanvas;
+            skiaCanvas.Save();
+            try
+            {
+                skiaCanvas.DrawRect(
+                    new SKRect(5f, 5f, (float)this.Bounds.Width - 10, (float)this.Bounds.Height - 10),
+                    _background);
+            }
+            finally
+            {
+                skiaCanvas.Restore();
+            }
+        }
+
+        /// <inheritdoc />
+        public bool Equals(ICustomDrawOperation? other)
+        {
+            return ReferenceEquals(this, other);
+        }
+    }
+}
---
+++ 2021/HappyCoding.AvaloniaSkia2DRendering/HappyCoding.AvaloniaSkia2DRendering/CustomSkiaRendererControl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
+using Avalonia.Media;
+using Avalonia.Threading;
+
+namespace HappyCoding.AvaloniaSkia2DRendering
+{
+    public class CustomSkiaRendererControl : Control
+    {
+        private CustomSkiaRenderer? _skiaRenderer;
+
+        public override void Render(DrawingContext context)
+        {
+            base.Render(context);
+
+            if (_skiaRenderer != null)
+            {
+                _skiaRenderer.Bounds = new Avalonia.Rect(0, 0, this.Bounds.Width, this.Bounds.Height);
+                context.Custom(_skiaRenderer);
+
+                //Dispatcher.UIThread.InvokeAsync(this.InvalidateVisual, DispatcherPriority.Background);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void OnAttachedToLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnAttachedToLogicalTree(e);
+
+            _skiaRenderer ??= new CustomSkiaRenderer();
+        }
+
+        /// <inheritdoc />
+        protected override void OnDetachedFromLogicalTree(LogicalTreeAttachmentEventArgs e)
+        {
+            base.OnDetachedFromLogicalTree(e);
+
+            _skiaRenderer?.Dispose();
+            _skiaRenderer = null;
+        }
+    }
+}
