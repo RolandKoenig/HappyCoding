db0ee8a325ef06780234f35e20ea74cd8df30d58
Roland König <roland.koenig@rolandk.de>
2021-07-14 16:07:59 +0200

    HappyCoding.AvaloniaMarkdownHelpBrowser: Added benchmark for SplitZeroAlloc

---
+++ 2021/HappyCoding.AvaloniaMarkdownHelpBrowser/HappyCoding.AvaloniaMarkdownHelpBrowser.Bench/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace HappyCoding.AvaloniaMarkdownHelpBrowser.Bench
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var summary = BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
---
+++ 2021/HappyCoding.AvaloniaMarkdownHelpBrowser/HappyCoding.AvaloniaMarkdownHelpBrowser.Bench/StringSplitBenchmark.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using HappyCoding.AvaloniaMarkdownHelpBrowser.DocFramework;
+
+namespace HappyCoding.AvaloniaMarkdownHelpBrowser.Bench
+{
+    [MemoryDiagnoser]
+    public class StringSplitBenchmark
+    {
+        [Benchmark]
+        public void SimpleCase_Normal()
+        {
+            foreach (var actSplitted in "Test1.Test2.Test3".Split('.'))
+            {
+               
+            }
+        }
+
+        [Benchmark]
+        public void SimpleCase_ZeroAlloc()
+        {
+            foreach (var actSplitted in "Test1.Test2.Test3".SplitZeroAlloc('.'))
+            {
+               
+            }
+        }
+    }
+}
