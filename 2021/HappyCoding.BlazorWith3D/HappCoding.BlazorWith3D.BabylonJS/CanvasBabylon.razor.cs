8dfa8d46bf7fc02f405d7e7a376a6af3124b8906
Roland König <roland.koenig@rolandk.de>
2021-07-29 09:16:20 +0200

    HappyCoding.BlazorWith3D: Initial commit

---
+++ 2021/HappyCoding.BlazorWith3D/HappCoding.BlazorWith3D.BabylonJS/CanvasBabylon.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyCoding.BlazorWith3D.BabylonJS
+{
+    public partial class CanvasBabylon
+    {
+        
+    }
+}
