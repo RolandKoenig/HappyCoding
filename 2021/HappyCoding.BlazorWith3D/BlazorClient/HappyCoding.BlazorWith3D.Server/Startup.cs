65c55d31f6ce0b71d1df29d369ddcf472dfb778d
Roland König <roland.koenig@rolandk.de>
2021-09-23 15:54:48 +0200

    HappyCoding.BlazorWith3D.Server: Small configuration changes

--- 2021/HappyCoding.BlazorWith3D/BlazorClient/HappyCoding.BlazorWith3D.Server/Startup.cs
+++ 2021/HappyCoding.BlazorWith3D/BlazorClient/HappyCoding.BlazorWith3D.Server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Linq;
 using HappyCoding.BlazorWith3D.Server.Util;
+using Microsoft.AspNetCore.HttpOverrides;
 
 namespace HappyCoding.BlazorWith3D.Server
 {
@@ -29,8 +30,21 @@ namespace HappyCoding.BlazorWith3D.Server
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // According to https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/linux-apache?view=aspnetcore-5.0
+            // Se are located behind an apache in production
+            if (!env.IsDevelopment())
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+            }
+
+            // Log every incoming request
             app.UseMiddleware<RequestLoggingMiddleware>();
-            app.UsePathBase("/blazorwith3d/");
+
+            // This application is below the path /blazorwith3d
+            app.UsePathBase("/blazorwith3d");
 
             if (env.IsDevelopment())
             {
