b9bf6c14b369b3c1b8a56efaedf82bcc79a70da2
Roland König <roland.koenig@rolandk.de>
2021-10-09 12:11:25 +0200

    HappyCoding.WinUISimple: Initial Checkin

---
+++ 2021/HappyCoding.WinUISimple/HappyCoding.WinUISimple/HappyCoding.WinUISimple/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace HappyCoding.WinUISimple
+{
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindowViewModel ViewModel { get; } = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            this.InitializeComponent();
+        }
+
+        private void myButton_Click(object sender, RoutedEventArgs e)
+        {
+            myButton.Content = "Clicked";
+        }
+    }
+}
---
+++ 2021/HappyCoding.WinUISimple/HappyCoding.WinUISimple/HappyCoding.WinUISimple/MainWindowViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HappyCoding.WinUISimple.Util;
+
+namespace HappyCoding.WinUISimple
+{
+    public class MainWindowViewModel : PropertyChangedBase
+    {
+        public string Title => "HappyCoding WinUISimple";
+    }
+}
