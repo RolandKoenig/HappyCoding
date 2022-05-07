1983a8339def2a9741b8c0e3c03449c8a830555f
Roland König <roland.koenig@rolandk.de>
2022-05-07 11:51:51 +0200

    HappyCoding.EFIncludePerformance: Additional cases for checking Include performance

---
+++ 2022/HappyCoding.EFIncludePerformance/HappyCoding.EFIncludePerformanceNet6/Model/ProcessActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyCoding.EFIncludePerformance.Model
+{
+    public class ProcessActivity
+    {
+        public string ProcessID { get; init; }
+
+        public long ProcessActivityID { get; init; }
+
+        public DateTimeOffset ActivityTimetamp { get; init; }
+
+        public int Field1 { get; init; }
+
+        public int Field2 { get; init; }
+
+        public int Field3 { get; init; }
+        
+        public string Field4 { get; init; }
+
+        public string Field5 { get; init; }
+
+        public int Field6 { get; set; }
+
+        public byte[] Field7 { get; set; }
+    }
+}
