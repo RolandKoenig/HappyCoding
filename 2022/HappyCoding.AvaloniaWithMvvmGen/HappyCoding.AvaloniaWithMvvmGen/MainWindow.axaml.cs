97de438ec4c8dcf89cc3ba56a50da7769f98ee1b
Roland König <roland.koenig@rolandk.de>
2022-05-09 21:27:13 +0200

    HappyCoding.AvaloniaWithMvvmGen: Initial checkin

---
+++ 2022/HappyCoding.AvaloniaWithMvvmGen/HappyCoding.AvaloniaWithMvvmGen/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HappyCoding.AvaloniaWithMvvmGen;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        this.InitializeComponent();
+    }
+}
---
+++ 2022/HappyCoding.AvaloniaWithMvvmGen/HappyCoding.AvaloniaWithMvvmGen/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MvvmGen;
+
+namespace HappyCoding.AvaloniaWithMvvmGen;
+
+[ViewModel]
+public partial class MainWindowViewModel
+{
+    [Property]
+    private string _title;
+
+    partial void OnInitialize()
+    {
+        this.Title = "⚡Avalonia with MvvmGen";
+    }
+}
