9ca87b4bd6327cf02596b1abcda47fc36afcd19e
Roland König <roland.koenig@rolandk.de>
2022-06-08 06:21:55 +0200

    HappyCoding.SeeingSharp2WithPicking: Initial checkin

---
+++ 2022/HappyCoding.SeeingSharp2WithPicking/HappyCoding.SeeingSharp2WithPicking/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Numerics;
+using SeeingSharp.Components.Input;
+using SeeingSharp.Core.Animations;
+using SeeingSharp.Drawing3D;
+using SeeingSharp.Drawing3D.Primitives;
+using SeeingSharp.Drawing3D.Resources;
+using SeeingSharp.Mathematics;
+
+namespace HappyCoding.SeeingSharp2WithPicking
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+
+            this.Activated += OnActivated;
+        }
+
+        private async void OnActivated(object sender, WindowActivatedEventArgs args)
+        {
+            // Build the scene
+            var scene = this.CtrlView3D.Scene;
+            await scene.ManipulateSceneAsync(manipulator =>
+            {
+                // Create material resource
+                var resMaterial = manipulator.AddStandardMaterialResource();
+                var resGeometry = manipulator.AddResource(
+                    device => new GeometryResource(new CubeGeometryFactory()));
+
+                // Create the mesh and animate it
+                var mesh = new Mesh(resGeometry, resMaterial);
+                mesh.Position = new Vector3(0f, 1f, 0f);
+                mesh.Color = Color4.GreenColor;
+                mesh.BuildAnimationSequence()
+                    .RotateEulerAnglesTo(new Vector3(0f, EngineMath.RAD_180DEG, 0f), TimeSpan.FromSeconds(2.0))
+                    .WaitFinished()
+                    .RotateEulerAnglesTo(new Vector3(0f, EngineMath.RAD_360DEG, 0f), TimeSpan.FromSeconds(2.0))
+                    .WaitFinished()
+                    .CallAction(() => mesh.RotationEuler = Vector3.Zero)
+                    .ApplyAndRewind();
+                manipulator.AddObject(mesh);
+            });
+
+            // Configure camera
+            var camera = this.CtrlView3D.Camera;
+            camera.Position = new Vector3(3f, 3f, 3f);
+            camera.Target = new Vector3(0f, 0.5f, 0f);
+            camera.UpdateCamera();
+
+            // Append camera behavior (this enables simple input / movement)
+            this.CtrlView3D.RenderLoop.SceneComponents.Add(new FreeMovingCameraComponent());
+        }
+    }
+}
