e444a8f5865b69869ba959415a883de2922f1898
Roland König <roland.koenig@rolandk.de>
2022-12-31 11:26:43 +0100

    HappyCoding.AvaloniaFluentThemeSwitch: Small refactoring

--- 2022/HappyCoding.AvaloniaFluentThemeSwitch/HappyCoding.AvaloniaFluentThemeSwitch/App.axaml.cs
+++ 2022/HappyCoding.AvaloniaFluentThemeSwitch/HappyCoding.AvaloniaFluentThemeSwitch/App.axaml.cs
@@ -2,7 +2,6 @@ using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Avalonia.Markup.Xaml.Styling;
 using Avalonia.Themes.Fluent;
 
 namespace HappyCoding.AvaloniaFluentThemeSwitch;
@@ -10,11 +9,10 @@ namespace HappyCoding.AvaloniaFluentThemeSwitch;
 public partial class App : Application
 {
     private static readonly FluentTheme s_fluentTheme = new(new Uri("avares://ControlCatalog/Styles"));
-    private static readonly StyleInclude s_dataGridFluentStyle = new(new Uri("avares://ControlCatalog/Styles"))
-    {
-        Source = new Uri("avares://Avalonia.Controls.DataGrid/Themes/Fluent.xaml")
-    };
 
+    /// <summary>
+    /// Manually sets current theme.
+    /// </summary>
     public static void SetFluentThemeMode(FluentThemeMode mode)
     {
         s_fluentTheme.Mode = mode;
@@ -23,7 +21,6 @@ public partial class App : Application
     public override void Initialize()
     {
         this.Styles.Insert(0, s_fluentTheme);
-        this.Styles.Insert(1, s_dataGridFluentStyle);
 
         AvaloniaXamlLoader.Load(this);
     }
