2d3e36df5e17f07a53d8d4c2c320d92d0723a1d6
Roland König <roland.koenig@rolandk.de>
2023-03-24 16:33:49 +0100

    HappyCoding.GrpcCommunicationFeatures: Initial checkin

---
+++ 2023/HappyCoding.GrpcCommunicationFeatures/HappyCoding.GrpcCommunicationFeatures.Server/GrpcServices/GreeterService.cs
@@ -0,0 +1,21 @@
+using Grpc.Core;
+using HappyCoding.GrpcCommunicationFeatures.Shared;
+
+namespace HappyCoding.GrpcCommunicationFeatures.GrpcServices;
+
+internal class GreeterService : Greeter.GreeterBase
+{
+    private readonly ILogger<GreeterService> _logger;
+    public GreeterService(ILogger<GreeterService> logger)
+    {
+        _logger = logger;
+    }
+
+    public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new HelloReply
+        {
+            Message = "Hello " + request.Name
+        });
+    }
+}
