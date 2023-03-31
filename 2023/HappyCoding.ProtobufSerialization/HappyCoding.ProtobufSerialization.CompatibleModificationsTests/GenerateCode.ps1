a627cace3d719a69a29d9dc961957b5d36ffcd63
Roland König <roland.koenig@rolandk.de>
2023-03-31 20:13:31 +0200

    HappyCoding.ProtobufSerialization: Added some compatibility checks

---
+++ 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.CompatibleModificationsTests/GenerateCode.ps1
@@ -0,0 +1,15 @@
+﻿# Install Protobuf Compiler (protoc) first
+
+# Negative cases
+protoc Negative/ChangeFieldType/MyTestMessageOriginal.proto --csharp_out=./Negative/ChangeFieldType
+protoc Negative/ChangeFieldType/MyTestMessageUpdated.proto --csharp_out=./Negative/ChangeFieldType
+
+# Positive cases
+protoc Positive/AddedField/MyTestMessageOriginal.proto --csharp_out=./Positive/AddedField
+protoc Positive/AddedField/MyTestMessageUpdated.proto --csharp_out=./Positive/AddedField
+
+protoc Positive/ChangedFieldNames/MyTestMessageOriginal.proto --csharp_out=./Positive/ChangedFieldNames
+protoc Positive/ChangedFieldNames/MyTestMessageUpdated.proto --csharp_out=./Positive/ChangedFieldNames
+
+protoc Positive/RemovedField/MyTestMessageOriginal.proto --csharp_out=./Positive/RemovedField
+protoc Positive/RemovedField/MyTestMessageUpdated.proto --csharp_out=./Positive/RemovedField
---
+++ 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.CompatibleModificationsTests/Negative/ChangeFieldType/TestCase.cs
@@ -0,0 +1,30 @@
+﻿using Google.Protobuf;
+
+namespace HappyCoding.ProtobufSerialization.CompatibleModificationsTests.Negative.ChangeFieldType;
+
+public class TestCase
+{
+    [Fact]
+    public void ExecuteTestCase()
+    {
+        // Prepare original object
+        var original = new MyTestMessageOriginal();
+        original.FirstName = "Test FirstName";
+        original.LastName = "Test LastName";
+        original.Age = 8;
+        original.Emails.Add("test@test.com");
+        original.Emails.Add("test@test.de");
+
+        // Protobuf serialization
+        using var memStream = new MemoryStream(original.CalculateSize());
+        original.WriteTo(memStream);
+        var serializedBytes = memStream.ToArray();
+
+        // Deserialize using updated object
+        var updated = new MyTestMessageUpdated();
+        updated.MergeFrom(serializedBytes);
+
+        // Breaks here because 'age' was not deserialized because of the changed type int32 -> string
+        Assert.Equal(original.Age.ToString(), updated.Age);
+    }
+}
