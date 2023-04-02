e996c554aa8996667e5e4857518a208ffb01a603
Roland König <roland.koenig@rolandk.de>
2023-04-02 09:49:15 +0200

    HappyCoding.ProtobufSerialization: Added testcase 'ChangedMessageName'

---
+++ 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.CompatibleModificationsTests/ChangedMessageName/TestCase.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf;
+
+namespace HappyCoding.ProtobufSerialization.CompatibleModificationsTests.ChangedMessageName;
+
+public class TestCase
+{
+    [Fact]
+    public void ExecuteTestCase()
+    {
+        // Prepare original object
+        var original = new MyTestMessageOriginal();
+        original.FirstName = "Test FirstName";
+        original.LastName = "Test LastName";
+        original.Age = 8;
+        original.Emails.Add("test@test.com");
+        original.Emails.Add("test@test.de");
+
+        // Protobuf serialization
+        using var memStream = new MemoryStream(original.CalculateSize());
+        original.WriteTo(memStream);
+        var serializedBytes = memStream.ToArray();
+
+        // Deserialize using updated object
+        var updated = new MyTestMessageUpdated();
+        updated.MergeFrom(serializedBytes);
+
+        // Asserts
+        Assert.Equal(original.FirstName, updated.FirstName);
+        Assert.Equal(original.LastName, updated.LastName);
+        Assert.Equal(original.Age, updated.Age);
+        Assert.Equal(original.Emails, updated.Emails);
+    }
+}
