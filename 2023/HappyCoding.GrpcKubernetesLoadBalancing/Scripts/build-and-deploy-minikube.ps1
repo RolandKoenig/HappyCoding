2b971c2529064c42d7f126a592f12afccf81e679
Roland König <roland.koenig@rolandk.de>
2023-04-24 07:00:03 +0200

    HappyCoding.GrpcKubernetesLoadBalancing: Updated build-and-deploy-minikube.ps1

--- 2023/HappyCoding.GrpcKubernetesLoadBalancing/Scripts/build-and-deploy-minikube.ps1
+++ 2023/HappyCoding.GrpcKubernetesLoadBalancing/Scripts/build-and-deploy-minikube.ps1
@@ -1,2 +1,4 @@
+# Build image in minikube
+# see https://levelup.gitconnected.com/two-easy-ways-to-use-local-docker-images-in-minikube-cd4dcb1a5379
 minikube image build -t happycodinggrpckubernetesloadbalancing-grpc_server ../HappyCoding.GrpcKubernetesLoadBalancing.Server
 minikube image build -t happycodinggrpckubernetesloadbalancing-grpc_client ../HappyCoding.GrpcKubernetesLoadBalancing.Client
