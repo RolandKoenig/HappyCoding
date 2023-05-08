267776e4fa6e0d2e077a1dad022dbebe90dc017e
Roland König <roland.koenig@rolandk.de>
2023-05-08 13:14:11 +0200

    HappyCoding.ProtobufSerialization: Added case for oneof

--- 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.UsingGoogleProtobuf/Data/GenerateCode.ps1
+++ 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.UsingGoogleProtobuf/Data/GenerateCode.ps1
@@ -1,6 +1,4 @@
 ﻿# Install Protobuf Compiler (protoc) first
 
 protoc MyTestMessage.proto --csharp_out=.
-protoc MyTestMessage_DifferentNames.proto --csharp_out=.
-protoc MyTestMessage_LessProperties.proto --csharp_out=.
-protoc MyTestMessage_MoreProperties.proto --csharp_out=.
+protoc MyTestMessageWithOneOf.proto --csharp_out=.
--- 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.UsingGoogleProtobuf/Program.cs
+++ 2023/HappyCoding.ProtobufSerialization/HappyCoding.ProtobufSerialization.UsingGoogleProtobuf/Program.cs
@@ -9,13 +9,21 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var myMessage = new MyTestMessage();
+        // // MyTestMessage
+        // var myMessage = new MyTestMessage();
+        // myMessage.FirstName = "Test FirstName";
+        // myMessage.LastName = "Test LastName";
+        // myMessage.Age = 8;
+        // myMessage.Emails.Add("test@test.com");
+        // myMessage.Emails.Add("test@test.de");
+
+        // MyTestMessageWithOneOf
+        var myMessage = new MyTestMessageWithOneOf();
         myMessage.FirstName = "Test FirstName";
         myMessage.LastName = "Test LastName";
         myMessage.Age = 8;
-        myMessage.Emails.Add("test@test.com");
-        myMessage.Emails.Add("test@test.de");
-
+        myMessage.ContactMailAddress = "test@test.de";
+        
         // Json Serialization
         var serializedBytesJson = JsonSerializer.SerializeToUtf8Bytes(myMessage, new JsonSerializerOptions(JsonSerializerDefaults.Web));
         var serializedJson = Encoding.UTF8.GetString(serializedBytesJson);
@@ -31,36 +39,6 @@ public class Program
 
         Console.WriteLine($"Protobuf serialization ({serializedBytes.Length} bytes): ");
         Console.WriteLine(ToHexString(serializedBytes));
-        Console.WriteLine();
-
-        // Deserialize with more properties
-        var myTestMessageMoreProperties = new MyTestMessage_MoreProperties();
-        myTestMessageMoreProperties.MergeFrom(serializedBytes);
-
-        serializedBytesJson = JsonSerializer.SerializeToUtf8Bytes(myTestMessageMoreProperties, new JsonSerializerOptions(JsonSerializerDefaults.Web));
-        serializedJson = Encoding.UTF8.GetString(serializedBytesJson);
-        Console.WriteLine($"Deserialized with more properties:");
-        Console.WriteLine(serializedJson);
-        Console.WriteLine();
-
-        // Deserialize with less properties
-        var myTestMessageLessProperties = new MyTestMessage_LessProperties();
-        myTestMessageLessProperties.MergeFrom(serializedBytes);
-
-        serializedBytesJson = JsonSerializer.SerializeToUtf8Bytes(myTestMessageLessProperties, new JsonSerializerOptions(JsonSerializerDefaults.Web));
-        serializedJson = Encoding.UTF8.GetString(serializedBytesJson);
-        Console.WriteLine($"Deserialized with less properties:");
-        Console.WriteLine(serializedJson);
-        Console.WriteLine();
-
-        // Deserialize with different names
-        var myTestMessageDifferentNames = new MyTestMessage_DifferentNames();
-        myTestMessageDifferentNames.MergeFrom(serializedBytes);
-
-        serializedBytesJson = JsonSerializer.SerializeToUtf8Bytes(myTestMessageDifferentNames, new JsonSerializerOptions(JsonSerializerDefaults.Web));
-        serializedJson = Encoding.UTF8.GetString(serializedBytesJson);
-        Console.WriteLine($"Deserialized with different names:");
-        Console.WriteLine(serializedJson);
         Console.WriteLine();
     }
 
