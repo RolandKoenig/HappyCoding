cf1e88b0c9c36f97ccc58a4ba22d07ca6d785aaa
Roland König <roland.koenig@rolandk.de>
2023-05-13 22:59:55 +0200

    HappyCoding.GrpcWithNet48: Initial checkin

---
+++ 2023/HappyCoding.GrpcWithNet48/HappyCoding.GrpcWithNet48.AspNetCoreServer/Program.cs
@@ -0,0 +1,21 @@
+using HappyCoding.GrpcWithNet48.AspNetCoreServer.Services;
+
+namespace HappyCoding.GrpcWithNet48.AspNetCoreServer;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddGrpc();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        app.UseGrpcWeb(new GrpcWebOptions(){ DefaultEnabled = true });
+        app.MapGrpcService<GreeterService>();
+
+        app.Run();
+    }
+}
---
+++ 2023/HappyCoding.GrpcWithNet48/HappyCoding.GrpcWithNet48.Net48Client/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using HappyCoding.GrpcWithNet48.ProtoDefinitions;
+
+namespace HappyCoding.GrpcWithNet48.Net48Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpClient = new HttpClient(
+                new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+
+            var baseUri = new Uri("http://localhost:5000");
+            var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+
+            var grpcClient = new Greeter.GreeterClient(channel);
+
+            for (var loop = 0; loop < 10; loop++)
+            {
+                var actName = $"Net48 Client {(loop + 1)}";
+
+                Console.WriteLine($"Sending hello from {actName}");
+                var response = await grpcClient.SayHelloAsync(
+                    new HelloRequest()
+                    {
+                        Name = actName
+                    });
+
+                Console.WriteLine($"Received: {response.Message}");
+
+                Console.WriteLine();
+                await Task.Delay(300);
+            }
+        }
+    }
+}
