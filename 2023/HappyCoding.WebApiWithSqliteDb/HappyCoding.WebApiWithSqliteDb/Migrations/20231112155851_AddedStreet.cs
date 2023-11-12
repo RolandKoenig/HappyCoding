0ad44c9b471acfb8ee0aaa6115ace1dd821d192b
Roland König <roland.koenig@rolandk.de>
2023-11-12 17:13:01 +0100

    HappyCoding.WebApiWithSqliteDb: Added missing files

---
+++ 2023/HappyCoding.WebApiWithSqliteDb/HappyCoding.WebApiWithSqliteDb/Migrations/20231112155851_AddedStreet.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyCoding.WebApiWithSqliteDb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedStreet : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Persons",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Persons");
+        }
+    }
+}
