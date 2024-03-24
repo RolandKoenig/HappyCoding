11ab8fb0346cd19e12d5d8e462967002e941a559
Roland König <roland.koenig@rolandk.de>
2024-03-24 16:24:07 +0100

    HappyCoding.EditorConfigPlayground: Full cleanup in rider

--- 2024/HappyCoding.EditorConfigPlayground/HappyCoding.EditorConfigPlayground/Controllers/WeatherForecastController.cs
+++ 2024/HappyCoding.EditorConfigPlayground/HappyCoding.EditorConfigPlayground/Controllers/WeatherForecastController.cs
@@ -4,19 +4,16 @@ namespace HappyCoding.EditorConfigPlayground.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+public class WeatherForecastController(ILogger<WeatherForecastController> logger) : ControllerBase
 {
+    private const string Version = "1.0.0";
+
     private static readonly string[] Summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
+    private readonly ILogger<WeatherForecastController> _logger = logger;
 
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
@@ -28,5 +25,13 @@ public class WeatherForecastController : ControllerBase
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+    }
+
+    [HttpGet("Version/{requestId}")]
+    public string GetVersion(int requestId)
+    {
+        if (requestId == 1) { return "Dummy"; }
+
+        return Version;
     }
 }
