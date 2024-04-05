420426aa0b6423acc590604b3e47e6fc2aabdac7
Roland König <roland.koenig@rolandk.de>
2024-04-05 06:47:00 +0200

    HappyCoding.CollectingMetrics: Collecting metrics

---
+++ 2024/HappyCoding.CollectingMetrics/HappyCoding.CollectingMetrics/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics.Metrics;
+
+public static class Program
+{
+    public static void Main()
+    {
+        var counterTask = ProduceMetricsAsync(CancellationToken.None);
+        
+        var listener = new MeterListener();
+        listener.InstrumentPublished += (instrument, meterListener) =>
+        {
+            if (instrument.Meter.Name is "MyMetrics")
+            {
+                meterListener.EnableMeasurementEvents(instrument);
+            }
+        };
+        listener.SetMeasurementEventCallback<int>((instrument, measurement, tags, state) =>
+        {
+            Console.WriteLine($"{instrument.Name} recorded measurement {measurement}");
+        });
+        listener.Start();
+        
+        Console.ReadLine();
+    }
+
+    private static async Task ProduceMetricsAsync(CancellationToken cancellationToken)
+    {
+        var meter = new Meter("MyMetrics", "1.0.0");
+        var counter1 = meter.CreateCounter<int>(
+            name: "counter1",
+            unit: "#",
+            description: "The number of hats sold in our store");
+
+        var randomizer = new Random(Environment.TickCount);
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            await Task.Delay(randomizer.Next(500, 1500));
+            
+            counter1.Add(randomizer.Next(5, 15));
+        }
+    }
+}
