439ea06fd914170bb4bbc349a4f564504dd5181c
Roland König <roland.koenig@rolandk.de>
2025-03-15 10:33:32 +0100

    HappyCoding.WpfWithMoreUiThreads: Initial commit

---
+++ 2025/HappyCoding.WpfWithMoreUiThreads/HappyCoding.WpfWithMoreUiThreads/Views/ThreadInfoViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace HappyCoding.WpfWithMoreUiThreads.Views;
+
+public partial class ThreadInfoViewModel : ObservableObject
+{
+    public int ThreadId => Thread.CurrentThread.ManagedThreadId;
+
+    [RelayCommand]
+    public async Task DoSomeAsyncWorkAsync()
+    {
+        await Task.Delay(5000);
+    }
+}
