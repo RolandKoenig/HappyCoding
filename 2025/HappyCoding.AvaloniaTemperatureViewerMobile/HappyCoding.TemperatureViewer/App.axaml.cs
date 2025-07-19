9a7786bbbcd430683eb3cccdcbca57457953128d
Roland König <roland.koenig@rolandk.de>
2025-07-19 16:56:54 +0200

    HappyCoding.TemperatureViewer.Mobile: Initial commit

---
+++ 2025/HappyCoding.AvaloniaTemperatureViewerMobile/HappyCoding.TemperatureViewer/App.axaml.cs
@@ -0,0 +1,35 @@
+using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using HappyCoding.TemperatureViewer.Services;
+
+namespace HappyCoding.TemperatureViewer;
+
+public partial class App : Application
+{
+    public override void Initialize()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+
+    public override void OnFrameworkInitializationCompleted()
+    {
+        var mainViewModel = new MainViewModel(new RandomMeasurementService());
+        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
+        {
+            var mainWindow = new MainWindow();
+            mainWindow.DataContext = mainViewModel;
+
+            desktop.MainWindow = mainWindow;
+        }
+        else if (ApplicationLifetime is ISingleViewApplicationLifetime singleViewApplicationLifetime)
+        {
+            var mainView = new MainView();
+            mainView.DataContext = mainViewModel;
+            
+            singleViewApplicationLifetime.MainView = mainView;
+        }
+
+        base.OnFrameworkInitializationCompleted();
+    }
+}
---
+++ 2025/HappyCoding.AvaloniaTemperatureViewerMobile/HappyCoding.TemperatureViewer/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace HappyCoding.TemperatureViewer;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
