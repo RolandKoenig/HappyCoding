b0fafd3f0a7e3d015d37a9ac27532809612c717d
Roland König <roland.koenig@rolandk.de>
2025-09-02 20:25:35 +0800

    HappyCoding.AvaloniaImageViewer: Added webp as supported format

--- 2025/HappyCoding.AvaloniaImageViewer/HappyCoding.AvaloniaImageViewer/ImageViewerConstants.cs
+++ 2025/HappyCoding.AvaloniaImageViewer/HappyCoding.AvaloniaImageViewer/ImageViewerConstants.cs
@@ -2,5 +2,5 @@ namespace HappyCoding.AvaloniaImageViewer;
 
 public static class ImageViewerConstants
 {
-    public static readonly string[] SUPPORTED_IMAGE_FORMATS = new string[] { ".jpg", ".jpeg", ".png", ".bmp" };
+    public static readonly string[] SUPPORTED_IMAGE_FORMATS = new string[] { ".jpg", ".jpeg", ".png", ".bmp", "webp" };
 }
